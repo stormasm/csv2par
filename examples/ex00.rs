2c76688cae0be73a57e5a5f364ca61f5c10c620c
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-12-05 09:43:03 -0800

    trim more options

--- examples/ex00.rs
+++ examples/ex00.rs
@@ -1,6 +1,6 @@
 use arrow::csv::ReaderBuilder;
 use clap::{Parser, ValueHint};
-use parquet::{arrow::ArrowWriter, errors::ParquetError, file::properties::WriterProperties};
+use parquet::{arrow::ArrowWriter, errors::ParquetError};
 use std::fs::File;
 use std::path::PathBuf;
 use std::sync::Arc;
@@ -31,38 +31,6 @@ struct Opts {
     /// Set the CSV file's column delimiter as a byte character.
     #[clap(short, long, default_value = ",")]
     delimiter: char,
-
-    /// Sets data page size limit.
-    #[clap(long)]
-    data_pagesize_limit: Option<usize>,
-
-    /// Sets dictionary page size limit.
-    #[clap(long)]
-    dictionary_pagesize_limit: Option<usize>,
-
-    /// Sets write batch size.
-    #[clap(long)]
-    write_batch_size: Option<usize>,
-
-    /// Sets max size for a row group.
-    #[clap(long)]
-    max_row_group_size: Option<usize>,
-
-    /// Sets "created by" property.
-    #[clap(long)]
-    created_by: Option<String>,
-
-    /// Sets flag to enable/disable dictionary encoding for any column.
-    #[clap(long)]
-    dictionary: bool,
-
-    /// Print the schema to stderr.
-    #[clap(short, long)]
-    print_schema: bool,
-
-    /// Only print the schema
-    #[clap(short = 'n', long)]
-    dry: bool,
 }
 
 fn main() -> Result<(), ParquetError> {
@@ -105,40 +73,7 @@ fn main() -> Result<(), ParquetError> {
         }
     }?;
 
-    if opts.print_schema || opts.dry {
-        let json = serde_json::to_string_pretty(&schema).unwrap();
-        eprintln!("Schema:");
-        println!("{}", json);
-        if opts.dry {
-            return Ok(());
-        }
-    }
-
-    let mut props = WriterProperties::builder().set_dictionary_enabled(opts.dictionary);
-
-    if let Some(size) = opts.write_batch_size {
-        props = props.set_write_batch_size(size);
-    }
-
-    if let Some(size) = opts.data_pagesize_limit {
-        props = props.set_data_pagesize_limit(size);
-    }
-
-    if let Some(size) = opts.dictionary_pagesize_limit {
-        props = props.set_dictionary_pagesize_limit(size);
-    }
-
-    if let Some(size) = opts.dictionary_pagesize_limit {
-        props = props.set_dictionary_pagesize_limit(size);
-    }
-
-    if let Some(size) = opts.max_row_group_size {
-        props = props.set_max_row_group_size(size);
-    }
-
-    if let Some(created_by) = opts.created_by {
-        props = props.set_created_by(created_by);
-    }
+    //    let mut props = WriterProperties::builder().set_dictionary_enabled(opts.dictionary);
 
     let schema_ref = Arc::new(schema);
 
@@ -151,7 +86,7 @@ fn main() -> Result<(), ParquetError> {
 
     let output = File::create(opts.output)?;
 
-    let mut writer = ArrowWriter::try_new(output, reader.schema(), Some(props.build()))?;
+    let mut writer = ArrowWriter::try_new(output, reader.schema(), None)?;
 
     for batch in reader {
         match batch {
