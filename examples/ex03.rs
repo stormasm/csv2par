ca8e4c102dcffe16dda6d5890889d6ff2d7f2168
Michael Angerman <1809991+stormasm@users.noreply.github.com>
2022-12-06 20:09:35 -0800

    ex03.rs is now hard coding the parquet file name so as to remove clap and the opts

---
+++ examples/ex03.rs
@@ -0,0 +1,61 @@
+use arrow::csv::ReaderBuilder;
+use parquet::{arrow::ArrowWriter, errors::ParquetError};
+use std::fs::File;
+use std::sync::Arc;
+
+fn main() -> Result<(), ParquetError> {
+    let data = vec![
+        vec!["id,entry"],
+        vec!["220101,john ran to the store"],
+        vec!["220102,bill ate cupcakes"],
+        vec!["220103,sue played in her sandbox"],
+        vec!["0,a"],
+        vec!["1,b"],
+        vec!["2,c"],
+    ];
+
+    let data = data
+        .iter()
+        .map(|x| x.join(","))
+        .collect::<Vec<_>>()
+        .join("\n");
+    let data = data.as_bytes();
+
+    let mut cursor = std::io::Cursor::new(data);
+
+    let delimiter: char = ',';
+
+    let schema =
+        match arrow::csv::reader::infer_file_schema(&mut cursor, delimiter as u8, None, true) {
+            Ok((schema, _inferred_has_header)) => Ok(schema),
+            Err(error) => Err(ParquetError::General(format!(
+                "Error inferring schema: {}",
+                error
+            ))),
+        }?;
+
+    let schema_ref = Arc::new(schema);
+
+    let builder = ReaderBuilder::new()
+        .has_header(true)
+        .with_delimiter(delimiter as u8)
+        .with_schema(schema_ref);
+
+    let reader = builder.build(cursor)?;
+
+    let output = File::create("foo.parquet")?;
+
+    let mut writer = ArrowWriter::try_new(output, reader.schema(), None)?;
+
+    for batch in reader {
+        match batch {
+            Ok(batch) => writer.write(&batch)?,
+            Err(error) => return Err(error.into()),
+        }
+    }
+
+    match writer.close() {
+        Ok(_) => Ok(()),
+        Err(error) => Err(error),
+    }
+}
